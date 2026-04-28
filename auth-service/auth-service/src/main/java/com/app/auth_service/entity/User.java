0b980690599c0f48b5c7401c0afb89188cd57747
mudit1024 <muditkumarbhatnagar@gmail.com>
2026-04-28 20:29:27 +0530

    feat(auth): add User entity with UUID and builder pattern

---
+++ auth-service/auth-service/src/main/java/com/app/auth_service/entity/User.java
@@ -0,0 +1,32 @@
+package com.app.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
