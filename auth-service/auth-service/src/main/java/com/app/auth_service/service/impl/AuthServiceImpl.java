467437d462efe6c0c2e2186fa25107c1c7b791a0
mudit1024 <muditkumarbhatnagar@gmail.com>
2026-04-28 22:05:22 +0530

    feat(auth): implement login logic with JWT generation

--- auth-service/auth-service/src/main/java/com/app/auth_service/service/impl/AuthServiceImpl.java
+++ auth-service/auth-service/src/main/java/com/app/auth_service/service/impl/AuthServiceImpl.java
@@ -6,10 +6,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.app.auth_service.dto.AuthResponse;
+import com.app.auth_service.dto.LoginRequest;
 import com.app.auth_service.dto.RegisterRequest;
 import com.app.auth_service.entity.User;
 import com.app.auth_service.repository.UserRepository;
 import com.app.auth_service.service.AuthService;
+import com.app.auth_service.util.JwtUtil;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Override
     public AuthResponse register(RegisterRequest request) {
@@ -35,4 +38,24 @@ public class AuthServiceImpl implements AuthService {
                 .role(user.getRole())
                 .build();
     }
+
+
+    @Override
+public AuthResponse login(LoginRequest request) {
+
+    User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+        throw new RuntimeException("Invalid credentials");
+    }
+
+    String token = jwtUtil.generateToken(user.getId(), user.getRole());
+
+    return AuthResponse.builder()
+            .accessToken(token)
+            .userId(user.getId().toString())
+            .role(user.getRole())
+            .build();
+}
 }
