7fd1b470da6e594c14f230f472418dd65305f395
mudit1024 <muditkumarbhatnagar@gmail.com>
2026-04-28 21:43:02 +0530

    feat(auth): add global exception handler for validation errors

--- auth-service/auth-service/src/main/java/com/app/auth_service/config/SecurityConfig.java
+++ auth-service/auth-service/src/main/java/com/app/auth_service/config/SecurityConfig.java
@@ -3,12 +3,26 @@ package com.app.auth_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable()) // disable CSRF for now (important for Postman)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll() // allow all auth endpoints
+                .anyRequest().authenticated() // everything else protected
+            );
+
+        return http.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
