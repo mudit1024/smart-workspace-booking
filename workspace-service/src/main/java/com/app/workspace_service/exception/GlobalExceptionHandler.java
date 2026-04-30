38e97e3909f11132eb7b25333e9654962ab8ed36
mudit1024 <muditkumarbhatnagar@gmail.com>
2026-05-01 00:55:06 +0530

    added one more exception handler

--- workspace-service/src/main/java/com/app/workspace_service/exception/GlobalExceptionHandler.java
+++ workspace-service/src/main/java/com/app/workspace_service/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.app.workspace_service.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
@@ -19,5 +23,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Something went wrong");
+    }
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
