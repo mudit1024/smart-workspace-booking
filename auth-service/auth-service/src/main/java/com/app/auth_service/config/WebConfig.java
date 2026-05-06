501c36e130e34c505faf88be26ec2d4907103d3c
mudit1024 <muditkumarbhatnagar@gmail.com>
2026-05-07 00:11:25 +0530

    added frontend production url for CORS

--- auth-service/auth-service/src/main/java/com/app/auth_service/config/WebConfig.java
+++ auth-service/auth-service/src/main/java/com/app/auth_service/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.app.auth_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +18,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
